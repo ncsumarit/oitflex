a9a4a66aa03fc34ad112ed3295c4bcd49c8f81f0
ncsumarit <jen_riehle@ncsu.edu>
2012-06-18 16:06:10 -0400

    fix point to js dir

--- header.php
+++ header.php
@@ -48,7 +48,7 @@
 	?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-<script src="<?php echo get_template_directory_uri(); ?>/js/jquery-1.7.2.min.js" type="text/javascript"></script>
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery-1.7.2.min.js" type="text/javascript"></script>
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
